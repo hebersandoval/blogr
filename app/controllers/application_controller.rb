6155230f7b370dbdd847ce52b06495c994b1dc4e
Heber Sandoval <he3ers@gmail.com>
2016-09-16 00:18:55 -0400

    Add simple layout for the blog application.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,4 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def test
-    render html: "Test, test, 1, 2, 3. It works!"
-  end
 end
